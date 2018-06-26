3648ec92fca8330d415b6ad0c316ddf00a3e1e58
otoolerj <beck.otoole@gmail.com>
2018-06-25 21:08:15 -0600

    commented plotting script

--- analysis/plot_var_dates.py
+++ analysis/plot_var_dates.py
@@ -11,26 +11,32 @@ from scipy import stats
 import numpy as np
 from get_df import get_df
 
-
 def plot_dates(x, y, labels):
+    """plot variable of interest versus date
+    """
     fig1 = plt.figure(1)
     ax1 = fig1.add_subplot(111)
     ax1.scatter(x,y)
 
 
 def main():
-    global df
+    """get dataframe from raw data, then plot all variables
+    versus date and output plots to the plots directory
+    """
+    #get dataframe from raw data
     df = get_df()
-    
+
+    #get list of variables to plot versus data
     variables = list(df)
     variables.remove('date')
+
+    #get x and y, then recursively plot all variables versus date 
     x = df['date'][0]
     for variable in variables:
         labels = ['date', variable]
         y = df[variable]
         plot_dates(x,y,labels)
-    
-    
+
+
 if __name__ == '__main__':
     main()
-    
