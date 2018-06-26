9d0ca28bb3d00b72a9405352ac9f0da2b6a2a136
otoolerj <beck.otoole@gmail.com>
2018-06-25 20:45:11 -0600

    commented

--- analysis/get_df.py
+++ analysis/get_df.py
@@ -5,19 +5,16 @@ Created on Fri Jun 15 22:06:50 2018
 @author: rjo40
 """
 
-import pandas as pd 
+import pandas as pd
 from os import listdir
 
 def get_df():
-    global df
-    def get_col_data(parameter, s):
-        """
-        pull out df values from file names 
-        """
-        return s[s.index(parameter)+len(parameter + '_'): s.index('_', s.index(parameter)+len(parameter + '_'))]
-    
-    #generate df from raw data files 
-    global files
+    """return a DataFrame that is generated from the Fitbit website and
+    contains the user's daily activity data (sleep and exersize) that will be
+    used for further analysis
+    """
+
+    #generate sleep and activity dataframe from raw data files
     files = listdir('../data')
     df = pd.DataFrame()
     df_sleep = pd.DataFrame()
@@ -27,11 +24,17 @@ def get_df():
             df = df.append(tmp_df, ignore_index = True)
         else:
             df_sleep = df_sleep.append(tmp_df, ignore_index = True)
+
+    #separate sleep date and time into seperate columns
     df_sleep['Date'] = df_sleep['Start Time'].apply(lambda row: row[:10])
     df_sleep['Start Time'] = df_sleep['Start Time'].apply(lambda row: row[11:])
     df_sleep['End Time'] = df_sleep['End Time'].apply(lambda row: row[11:])
+
+    #combine sleep and activity dataframe by common date
     col = ['Date']
     df = df.join(df_sleep.set_index(col), col)
+
+    #clean-up and format dataframe
     df = df.rename(columns = lambda x: x.replace(' ', '_').lower())
     df = df[df.distance != 0.0]
     df = df.dropna()
